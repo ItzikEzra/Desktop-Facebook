4ae3a965cb500fdbb198995634534c9f1518c99d
ItzikEzra <Itzikezra11@gmail.com>
2020-11-05 14:01:20 +0200

    Add files via upload

---
+++ FacebookManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+namespace Desktop_Facebook
+{
+    public class FacebookManager
+    {
+        public FacebookManager()
+        {
+
+        }
+
+        public User LoggedInUser { get; set; }
+
+        public LoginResult LoginResult { get; set; }
+
+        public void Login()
+        {
+            LoginResult = FacebookService.Login(
+                "2842375502752590",
+                "public_profile");
+
+            this.LoggedInUser = this.LoginResult.LoggedInUser;
+        }
+        public void Logout()
+        {
+            FacebookService.Logout(null);
+            this.LoggedInUser = null;
+            this.LoginResult = null;
+        }
+
+        public void Connect()
+        {
+            this.LoggedInUser = LoginResult.LoggedInUser;
+        }
+   
+     
+
+    }
+}
