ab6de569454edb8a508d6022383dafb6a021180a
ItzikEzra <Itzikezra11@gmail.com>
2020-11-09 21:13:25 +0200

    clean the code a bit

--- Form1.cs
+++ Form1.cs
@@ -55,39 +55,5 @@ namespace Desktop_Facebook
                 MessageBox.Show(ex.Message);
             }
         }
-        #region
-        private void labelProfileName_Click(object sender, EventArgs e)
-        {
-        }
-
-
-        private void FormFacebook_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelProfileName_Click_1(object sender, EventArgs e)
-        {
-
-        }
-        private void m_ProfilePicMain_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-        #endregion
     }
 }
